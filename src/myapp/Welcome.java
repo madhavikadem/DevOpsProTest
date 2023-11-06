3d53d543f86b8502aa28f8c0ae9976ccafb14acc
padma <padma@10.0.0.171>
2023-11-06 14:55:52 -0500

    Happy Msg

--- src/myapp/Welcome.java
+++ src/myapp/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to code Lab Technology");
 		System.out.println(50*60);
+		System.out.println("Hurray we did it");
 	}
 
 }
